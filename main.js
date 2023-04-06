7ad3a826472ae499c15d1cb2a2f410d6576186c0
Ayesha-Esmati <aishaesmati45@gmail.com>
2023-04-06 12:49:52 +0430

    Functionalities are added

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
 // {IIFE
 (function () {
-  "";
   // targiting the screen has class screen
   let screen = document.querySelector(".screen");
   let buttons = document.querySelectorAll(".btn");
+  let clear = document.querySelector(".btn-clear");
+  let equal = document.querySelector(".btn-equal");
+
+  // first functionality
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function (e) {
+      let userInput = e.target.dataset.num;
+      screen.value += userInput;
+    });
+  });
+
+  //second functionality
+  equal.addEventListener("click", function (e) {
+    if (screen.value === "") {
+      screen.value = "";
+    } else {
+      let answer = eval(screen.value);
+      screen.value = answer;
+    }
+  });
+
+  // Third fuctionality: clear screen
+  clear.addEventListener("click", function (e) {
+    screen.value = "";
+  });
 })();
